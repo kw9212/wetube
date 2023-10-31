d2c180fb56c383cbba300dc9c57592277b22fbd1
KeunWoo Song <ks3651@columbia.edu>
2023-10-30 23:36:11 -0400

    added middlewares and routers

---
+++ README.md
@@ -0,0 +1,17 @@
+# Wetube Reloaded
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+user routers
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+video routers
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a video
+/videos/comments/delete -> Delete A Comment of a Video
--- src/server.js
+++ src/server.js
@@ -1,7 +1,34 @@
 import express from "express";
+import morgan from "morgan";
+
+const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
+
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () => console.log("sever listening on port 4000");
 
-app.listen(4000, handleListening);
+app.listen(PORT, handleListening);
