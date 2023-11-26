706ed410cb2f426617d8803269572ec0a1f56664
KeunWoo Song <ks3651@columbia.edu>
2023-11-25 20:38:51 -0500

    file uploads

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -146,10 +146,12 @@ export const getEdit = (req, res) => {
 export const postEdit = async (req, res) => {
   const {
     session: {
-      user: { _id, email: sessionEmail, username: sessionUsername },
+      user: { _id, avatarUrl, email: sessionEmail, username: sessionUsername },
     },
     body: { name, email, username, location },
+    file,
   } = req;
+
   let searchParam = [];
   if (sessionEmail != email) {
     searchParam.push({ email });
@@ -170,6 +172,7 @@ export const postEdit = async (req, res) => {
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
+      avatarUrl: file ? file.path : avatarUrl,
       name,
       email,
       username,
